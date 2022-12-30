64c90187bf2208c4fd718025e7677ce91821c7e9
wu55246842 <wuliang0118@gmail.com>
2022-12-30 22:21:14 +0800

    remove

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -9,40 +9,16 @@ const routes = [
         component: Container,
         children: [
             {
-                path: '/docs',
-                name: 'docs',
-                component: () => import('../views/Docs.vue'),
-                meta: { title: 'docs', keepAlive: false }
-            },
-            {
                 path: '/home',
                 name: 'home',
                 component: () => import('../views/Index.vue'),
                 meta: { title: 'home', keepAlive: false }
             },
             {
-                path: '/publish',
-                name: 'publish',
-                component: () => import('../views/Publish.vue'),
-                meta: { title: 'publish', keepAlive: false }
-            },
-            {
-                path: '/categories',
-                name: 'Blog Categories',
-                component: () => import('../views/Categories.vue'),
-                meta: { title: 'Blog Categories', keepAlive: false }
-            },
-            {
                 path: '/playground',
                 name: 'Playground',
                 component: () => import('../views/Playground.vue'),
                 meta: { title: 'Playground', keepAlive: false }
-            },
-            {
-                path: '/memmorial',
-                name: 'PetsMemorial',
-                component: () => import('../views/Memmorial.vue'),
-                meta: { title: 'Pets Memorial', keepAlive: false }
             },
             {
                 path: '/about',
@@ -58,49 +34,6 @@ const routes = [
             }
         ]
     },
-    {
-        path: "/",
-        name: "Read",
-        redirect: '/home',
-        component: Container,
-        children: [
-          {
-            path: '/read/:tokenId/:id',
-            name: 'Detail',
-            component: () => import('../views/Read.vue'),
-            meta: { title: 'Blog Detail', keepAlive: false }
-          }
-        ]
-    },
-    // {
-    //     path: "/",
-    //     name: "Table",
-    //     redirect: '/home',
-    //     component: Container,
-    //     children: [
-    //       {
-    //         path: '/table/:id',
-    //         name: 'Table',
-    //         component: () => import('../views/Table.vue'),
-    //         meta: { title: 'Table', keepAlive: false }
-    //       }
-    //     ]
-    // },
-    // {
-    //     path: "/",
-    //     name: "Table",
-    //     redirect: '/home',
-    //     component: Container,
-    //     children: [
-    //       {
-    //         path: '/table',
-    //         name: 'Table',
-    //         component: () => import('../views/Table.vue'),
-    //         meta: { title: 'Table', keepAlive: false }
-    //       }
-    //     ]
-    // },
-    
 
 ]
 
